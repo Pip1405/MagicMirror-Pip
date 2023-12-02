279bf4e280140c0f194ade6b63202b5c4f91fb2d
Pip1405 <131299882+Pip1405@users.noreply.github.com>
2023-12-02 12:24:09 +0100

    Add installation script and instructions

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Go to the modules directory
+cd modules
+
+# Clone each module
+git clone https://github.com/CFenner/MMM-AirQuality
+git clone https://github.com/lavolp3/MMM-AVStock
+git clone https://github.com/darickc/MMM-BackgroundSlideshow
+git clone https://github.com/werthdavid/MMM-Bring
+git clone https://github.com/MMRIZE/MMM-CalendarExt3
+git clone https://github.com/shbatm/MMM-Carousel
+git clone https://github.com/bibaldo/MMM-COVID19
+git clone https://github.com/LukeSkywalker92/MMM-DWD-WarnWeather
+git clone https://github.com/mykle1/MMM-EasyPix
+git clone https://github.com/jalibu/MMM-Jast
+git clone https://github.com/shbatm/MMM-KeyBindings
+git clone https://github.com/eouia/MMM-News
+git clone https://github.com/mumblebaj/MMM-NewsAPI
+git clone https://github.com/choffmann/MMM-Notion
+git clone https://github.com/MarcLandis/MMM-OpenWeatherMapForecast
+git clone https://github.com/jalibu/MMM-RAIN-MAP
+git clone https://github.com/fruestueck/MMM-WeatherDependentClothes
+
+# Go back to the project root
+cd ..
